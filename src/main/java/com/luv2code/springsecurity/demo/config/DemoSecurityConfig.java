4b1b1f3e20bdbf8c6ef30238ffc9ddfa8ce163c6
SprinchakDmitri <sprinchak.dmitriy@gmail.com>
2020-11-12 12:42:23 +0200

    JDBC authentication fix

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 import javax.sql.DataSource;
 
-@Configuration
+
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
